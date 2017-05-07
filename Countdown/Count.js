cee83a0c02010c5cf0d4de4d9dff4a496799fbaa
Mergjunarhola <jonbe209@nemi.tskoli.is>
2017-05-07 00:13:33 +0000

    Countdown tilbúið

--- Countdown/Count.js
+++ Countdown/Count.js
@@ -6,7 +6,7 @@
     var nuna = new Date();
     var time_length = 200;
     //maður getur breytt targetinu til að fá aðra tímasettningu
-    var target = new Date("201444-05-07");
+    var target = new Date("2017-05-07");
     var targethold = parseInt(target.getTime()/1000);
     
     var Sec = "";
@@ -25,44 +25,57 @@
         hold = targethold - hold;
         
         
+        if (hold<0){
+            Sec = "00";
+            Min = "00";
+            Hou = "00";
+            Dag = "00";
+            }
         
-        //Sekúntur
-        if ((hold%60)<10){
-            Sec="0"+parseInt((hold%60).toString());
-        }
         else{
-            Sec=(hold%60).toString();
-        }
+            
         
-        //Mínótur
-        if (((hold/60)%60)<10){
-            Min="0"+parseInt(((hold/60)%60).toString());
-        }
-        else{
-            Min=parseInt((hold/60)%60).toString();
-        }
-        
-        //Klukkutímar
-        if (((hold/60/60)%24)<10){
-            Hou="0"+parseInt(((hold/60/60)%24).toString());
-        }
-        else{
-            Hou=parseInt((hold/60/60)%24).toString();
-        }
-        
-        //Dagar
-        if ((hold/60/60/24)<10){
-            Dag="0"+parseInt((hold/60/60/24).toString());
-        }
-        else{
-            Dag=parseInt(hold/60/60/24).toString();
-        }
-        
-        
-        
-        
+            //Sekúntur
+            if ((hold % 60) < 10){
+                Sec="0"+parseInt((hold % 60).toString());
+            }
+            else{
+                Sec=(hold % 60).toString();
+            }
+            
+            //Mínótur
+            if (((hold / 60) % 60) < 10){
+                Min="0"+parseInt(((hold/60)%60).toString());
+            }
+            else{
+                Min=parseInt((hold / 60) % 60).toString();
+            }
+            
+            //Klukkutímar
+            if (((hold / 60 / 60) % 24) < 10){
+                Hou="0"+parseInt(((hold / 60 / 60) % 24).toString());
+            }
+            else{
+                Hou=parseInt((hold / 60 / 60) % 24).toString();
+            }
+            
+            //Dagar
+            if ((hold / 60 / 60 / 24)<10){
+                Dag="0"+parseInt((hold / 60 / 60 / 24).toString());
+            }
+            else{
+                Dag=parseInt(hold / 60 / 60 / 24).toString();
+            }
         document.getElementById("Dagarstat").innerHTML = Dag;
+        document.getElementById("Klukkstat").innerHTML = Hou;
+        document.getElementById("Minstat").innerHTML = Min;
+        document.getElementById("Sekstat").innerHTML = Sec;
         window.setTimeout("Countdown()",time_length);
+        }
+        
+        
+        
+        
     }
     Countdown();
     
